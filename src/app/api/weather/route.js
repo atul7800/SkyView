43146a75fa8ac11fb95369de1dfc6e8f19486bf9
atul <atulgupta.d796@gmil.com>
2025-05-29 23:16:37 +0530

    Coordinate access issue fixed

---
+++ src/app/api/weather/route.js
@@ -0,0 +1,25 @@
+import {NextRequest, NextResponse} from "next/server";
+
+//localhost:3000/api/weather
+export async function GET(request) {
+  const {searchParams} = new URL(request.url);
+  const WEATHER_API_KEY = process.env.OPEN_WEATHER_API;
+  const address = searchParams.get("address");
+  const latitude = searchParams.get("lat");
+  const longitude = searchParams.get("lon");
+  let url = "";
+  if (address) {
+    url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      address +
+      "&appid=" +
+      WEATHER_API_KEY;
+  } else {
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`;
+  }
+  console.log(url);
+  const res = await fetch(url);
+
+  const data = await res.json();
+  return NextResponse.json({data});
+}
